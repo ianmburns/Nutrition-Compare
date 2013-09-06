aa377dedbccf99c023c32d3450176a8038188d7d
ianmburns <ianmburns@gmail.com>
2013-09-05 22:06:18 -0400

    Fixed bundle with missing CSS by removing bundle

--- NutritionCompare/App_Start/BundleConfig.cs
+++ NutritionCompare/App_Start/BundleConfig.cs
@@ -17,8 +17,6 @@ namespace NutritionCompare
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
             BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
             BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/Content/bootstrap/bootstrap.css"));
             BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/Content/bootstrap/bootstrap-theme.css"));
